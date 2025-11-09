53ce1e31ebb264ca89bcb18f7209f04e6d9ff43c
Jenina <jeninakaija@gmail.com>
2025-11-09 18:05:38 +0200

    Added a small console program
    Joke console program fetches 3 jokes and prints them on the console. In the future the LMA app will use this program to show a joke/jokes on its main page.

---
+++ Notes/Notes/Joke/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Json;
+
+class Program
+{
+    static async Task Main()
+    {
+        using var httpClient = new HttpClient();
+
+        string url = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&amount=3";
+
+        // Fetches json data from the api and makes it into an object
+        var response = await httpClient.GetFromJsonAsync<JokeApiResponse>(url);
+
+        Console.WriteLine("Jokes:\n");
+
+        if (response?.Jokes != null)
+        {
+            int i = 1;
+            foreach (var j in response.Jokes)
+            {
+                Console.WriteLine($"{i++}. {j.Joke}\n");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No jokes found");
+        }
+    }
+}
+
+class JokeApiResponse
+{
+    public List<JokeItem>? Jokes { get; set; }
+}
+
+class JokeItem
+{
+    public string Joke { get; set; } = "";
+}
